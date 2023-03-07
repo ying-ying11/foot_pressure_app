5d9b03c9dbec6fecba90da43030a5742f6af6e8a
yingin <a0916476112@gmail.com>
2023-03-07 11:59:23 +0800

    modify male and famale calbriate

--- app/src/main/java/com/flyn/sarcopenia_project/file/cache_file/EmgCacheFile.kt
+++ app/src/main/java/com/flyn/sarcopenia_project/file/cache_file/EmgCacheFile.kt
@@ -1,9 +1,9 @@
 package com.flyn.sarcopenia_project.file.cache_file
 
-class EmgCacheFile(private val timeLabel: Long, private val emgData: ShortArray): CacheFile {
+class EmgCacheFile(private val timeLabel: Long, private val emgData: ShortArray, private val average: Double, private val isFatigue: Boolean): CacheFile {
 
     override fun toCsv(): String {
-        return "$timeLabel, ${emgData[0]}, ${emgData[1]}, ${emgData[2]}, ${emgData[3]}, ${emgData[4]}, ${emgData[5]}\n"
+        return "$timeLabel, ${emgData[0]}, ${emgData[1]}, ${emgData[2]}, ${emgData[3]}, ${emgData[4]}, ${emgData[5]}, $average, $isFatigue\n"
     }
 
 }
--- app/src/main/java/com/flyn/sarcopenia_project/viewer/DataViewer.kt
+++ app/src/main/java/com/flyn/sarcopenia_project/viewer/DataViewer.kt
@@ -1,13 +1,17 @@
 package com.flyn.sarcopenia_project.viewer
 
 
+import android.app.Service
 import android.content.*
 import android.os.Bundle
 import android.os.IBinder
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -42,7 +46,7 @@ class DataViewer: AppCompatActivity() {
         private val tagName = listOf("Foot Pressure")
     }
 
-    private val emg = DataPage(0f, 10000f, "Hallux","LT","M1","M5","Arch","HM") { "%.2f Pa".format(it) }
+    private val emg = DataPage(0f, 25000f, "Hallux","LT","M1","M5","Arch","HM") { "%.2f Pa".format(it) }
 //    private val emg = DataPage(0f, 4095f, "Hallux","LT","M1","M5","Arch","HM") { "%.2f v".format(emgTransform(it)) }
 //
 //    private val acc = DataPage(-32768f, 32768f, "x", "y", "z") { "%.2f g".format(accTransform(it)) }
@@ -62,6 +66,7 @@ class DataViewer: AppCompatActivity() {
     private val dataReceiver = object: BroadcastReceiver() {
 
         override fun onReceive(context: Context, intent: Intent) {
+
             when (intent.action) {
                 BleAction.GATT_CONNECTED.name -> {
                     service.enableNotification(true)
@@ -80,26 +85,42 @@ class DataViewer: AppCompatActivity() {
                         val adc = shortArrayOf(data.short, data.short, data.short, data.short, data.short, data.short)
 //                        Log.i("adc",adc.toList().toString())
 
-                        val ha_gram = calculatePa(calibrate(emgTransform(adc[0]), 0)-247.73,0)//right
-                        val lt_gram = calculatePa(calibrate(emgTransform(adc[1]), 1)-177.81,1)
-                        val m1_gram = calculatePa(calibrate(emgTransform(adc[2]), 2)-427.71,2)
-                        val m5_gram = calculatePa(calibrate(emgTransform(adc[3]), 3)-131.9,3)
-                        val arch_gram = calculatePa(calibrate(emgTransform(adc[4]), 4)+90.28,4)
-                        val hm_gram = calculatePa(calibrate(emgTransform(adc[5]), 5)-361.34,5)
-//                        val ha_gram = calculatePa(calibrate(emgTransform(adc[0]), 0)+20.96,0)//left
-//                        val lt_gram = calculatePa(calibrate(emgTransform(adc[1]), 1)-61.18,1)
-//                        val m1_gram = calculatePa(calibrate(emgTransform(adc[2]), 2)-15.05,2)
-//                        val m5_gram = calculatePa(calibrate(emgTransform(adc[3]), 3)+69.34,3)
-//                        val arch_gram = calculatePa(calibrate(emgTransform(adc[4]), 4)+55.22,4)
-//                        val hm_gram = calculatePa(calibrate(emgTransform(adc[5]), 5)+51.35,5)
+
+//                        val ha_gram = calculatePa(calibrate(emgTransform(adc[0]), 0)+260,0)// male right
+//                        val lt_gram = calculatePa(calibrate(emgTransform(adc[1]), 1)+730,1)
+//                        val m1_gram = calculatePa(calibrate(emgTransform(adc[2]), 2)+460,2)
+//                        val m5_gram = calculatePa(calibrate(emgTransform(adc[3]), 3)+430,3)
+//                        val arch_gram = calculatePa(calibrate(emgTransform(adc[4]), 4)+565,4)
+//                        val hm_gram = calculatePa(calibrate(emgTransform(adc[5]), 5)+145,5)
+//
+//                        val ha_gram = calculatePa(calibrate(emgTransform(adc[0]), 0)+95,0)// male left
+//                        val lt_gram = calculatePa(calibrate(emgTransform(adc[1]), 1)+390,1)
+//                        val m1_gram = calculatePa(calibrate(emgTransform(adc[2]), 2)+85,2)
+//                        val m5_gram = calculatePa(calibrate(emgTransform(adc[3]), 3)+550,3)
+//                        val arch_gram = calculatePa(calibrate(emgTransform(adc[4]), 4)+350,4)
+//                        val hm_gram = calculatePa(calibrate(emgTransform(adc[5]), 5)+100,5)
+
+//                        val ha_gram = calculatePa(calibrate(emgTransform(adc[0]), 0),0)-1500// female right
+//                        val lt_gram = calculatePa(calibrate(emgTransform(adc[1]), 1),1)-600
+//                        val m1_gram = calculatePa(calibrate(emgTransform(adc[2]), 2),2)-1200
+//                        val m5_gram = calculatePa(calibrate(emgTransform(adc[3]), 3),3)-1200
+//                        val arch_gram = calculatePa(calibrate(emgTransform(adc[4]), 4),4)-400
+//                        val hm_gram = calculatePa(calibrate(emgTransform(adc[5]), 5),5)+300
+//
+                        val ha_gram = calculatePa(calibrate(emgTransform(adc[0]), 0),0)-300// female left
+                        val lt_gram = calculatePa(calibrate(emgTransform(adc[1]), 1),1)-250
+                        val m1_gram = calculatePa(calibrate(emgTransform(adc[2]), 2),2)-800
+                        val m5_gram = calculatePa(calibrate(emgTransform(adc[3]), 3),3)
+                        val arch_gram = calculatePa(calibrate(emgTransform(adc[4]), 4),4)-50
+                        val hm_gram = calculatePa(calibrate(emgTransform(adc[5]), 5),5)-300
+
 //                        val ha_gram = calibrate(emgTransform(adc[0]), 0)
 //                        val lt_gram = calibrate(emgTransform(adc[1]), 1)
 //                        val m1_gram = calibrate(emgTransform(adc[2]), 2)
 //                        val m5_gram = calibrate(emgTransform(adc[3]), 3)
 //                        val arch_gram = calibrate(emgTransform(adc[4]), 4)
 //                        val hm_gram = calibrate(emgTransform(adc[5]), 5)
-
-
+                        Log.i("test", adc.toList().toString())
 
 
 
@@ -110,18 +131,18 @@ class DataViewer: AppCompatActivity() {
 
 //                        val text = getString(R.string.emg_describe,emgTransform(adc1),emgTransform(adc2),emgTransform(adc3),emgTransform(adc4),emgTransform(adc5),emgTransform(adc6))
                         emg.addData(text, ha_gram.toFloat(), lt_gram.toFloat(), m1_gram.toFloat(), m5_gram.toFloat(), arch_gram.toFloat(), hm_gram.toFloat())
-                        GlobalScope.launch(Dispatchers.IO) {
-                            FileManager.appendRecordData(FileManager.ADC1_FILE, EmgCacheFile(time, adc))
-                        }
 
-                        appendData(ha_gram)
+                        datalist.add(ha_gram)
 //                        val fp = FindPeak(mutableListOfDouble.toDoubleArray())//mutableList
+
                         val fp = FindPeak(datalist.toDoubleArray())//linked list
                         val out: Peak = fp.detectPeaks()
+                        var isFatigue = false
 //                        val peaks: IntArray = out.getPeaks()
 //                        val height = out.findPeakHeights(peaks)
-                        filteredPeaks1 = out.filterByHeight(2500.0, 20000.0)
-                        filteredPeaks_t = out.filterByPeakDistance(filteredPeaks1,100)
+                        filteredPeaks1 = out.filterByHeight(1000.0, 20000.0)
+                        filteredPeaks_t = out.filterByPeakDistance(filteredPeaks1,50)
+//                        fpArray.add(filteredPeaks_t.last())
                         height = out.findPeakHeights(filteredPeaks_t)
                         if (filteredPeaks_t.toList().isNotEmpty()){
 //                            filteredPeaks2.add(height.toList().last().toInt())
@@ -130,25 +151,56 @@ class DataViewer: AppCompatActivity() {
 //                                filteredPeaks2.add(height.toList().last().toInt())
 //
 //                            }
-
 //                            filteredPeaks2.add(i,height.toList().last().toInt())
 //                            i++
+
                             emg.updateSamplingRate(++adcCount, height.toList().last(),(filteredPeaks_t.toList().last().toDouble()/1000))
+
                         }
                         else{
                             emg.updateSamplingRate(++adcCount, 0.0, 0.0)
                         }
-                        if (datalist.size >6000){
+                        if (datalist.size >= 1000){
+                            Log.i("height", height.toList().toString())
+                            if (time - time_start >= 10000){  //10000、15000、200000 By experimental protocol testing App whether detecting fatigue or not
+                                if(height.isNotEmpty()){
+                                    heightAverage = height.average()
+                                    PressureList.add(heightAverage)
+                                    if(PressureList.size >= 2){
+                                        if(PressureList.get(PressureList.size-1)<PressureList.first && PressureList.get(PressureList.size-1)<PressureList.get(PressureList.size-2)){
+                                            i++
+                                            if(i>=3){
+                                                isFatigue = true
+                                                Toast.makeText(this@DataViewer,"You will Fatigue",Toast.LENGTH_LONG).show()
+                                                vibrator = getSystemService(Service.VIBRATOR_SERVICE) as Vibrator
+                                                vibrationEffect = VibrationEffect.createOneShot(1000, 150)
+                                                vibrator.vibrate(vibrationEffect)
+//                                                vibrator.vibrate(1000)
+                                                i = 0
+                                            }
+                                        }
+                                        else{
+                                            i = 0
+                                        }
+                                    }
+                                }
+                                time_start = time
+                            }
                             datalist.removeFirst()
+//                            datalist.clear()
                         }
-                        Log.i("peak",filteredPeaks2.toList().toString())
-//                        Log.i("count",adcCount.toString())
+//                        Log.i("peak",PressureList.toList().toString())
+//                        Log.i("count",datalist.size.toString())
 //                        Log.i("ha",ha_gram.toString())
 //                        adc_double = doubleArrayOf(0.0)
 //                        adc_double.add
 //                        Log.i("array",mutableListOfDouble.toString())
 //                        Log.i("lenght",mutableListOfDouble.size.toString())
 //                        Log.i("count",adcCount.toString())
+
+                        GlobalScope.launch(Dispatchers.IO) {
+                            FileManager.appendRecordData(FileManager.ADC1_FILE, EmgCacheFile(time, adc, heightAverage, isFatigue))
+                        }
 
                     }
                 }
@@ -161,6 +213,7 @@ class DataViewer: AppCompatActivity() {
 
         override fun onServiceConnected(name: ComponentName?, binder: IBinder?) {
             service = (binder as BluetoothLeService.BleServiceBinder).getService()
+
             service.connect(address)
         }
 
@@ -179,13 +232,18 @@ class DataViewer: AppCompatActivity() {
     private val saveButton: FloatingActionButton by lazy { findViewById(R.id.data_viewer_save_button) }
 //    private val analysisButton: Button by lazy {findViewById(R.id.analysis_foot_button)}
 //    private lateinit var analysisButton: Button
+    lateinit var vibrator: Vibrator
+    
+    lateinit var vibrationEffect: VibrationEffect
     private lateinit var address: String
     private lateinit var service: BluetoothLeService
     private lateinit var height : DoubleArray
+    private var heightAverage : Double = 0.0
     private lateinit var filteredPeaks1 : IntArray
     private lateinit var filteredPeaks_t : IntArray
-
-    private var filteredPeaks2 : MutableList<Int> = mutableListOf()
+    private lateinit var fpArray : LinkedList<Int>
+    private var time_start: Long = 0
+    private var analysis_peakvalue : MutableList<Double> = mutableListOf()
     private var step_duration : MutableList<Double> = mutableListOf()
     private var adcCount = 0
     private var i=0
@@ -195,15 +253,23 @@ class DataViewer: AppCompatActivity() {
 //    private var accCount = 0
 //    private var gyrCount = 0
 
-    private fun emgTransform(value: Short): Double = value.toDouble() / 4095f * 3.6f
+    private fun emgTransform(value: Short): Double = (value.toDouble() / 4095f) * 3.6f
     private fun emgTransform(value: Float): Float = value / 4095f * 3.6f
 ////
-//    private val weight = arrayOf(0.0019, 0.0015, 0.0018, 0.0022, 0.0019, 0.0021) // left
-//    private val bias = arrayOf(-0.0284, 0.0487, 0.0306, -0.1587, -0.1181, -0.1096)
-    private val weight = arrayOf(0.0014, 0.0008, 0.0016, 0.001, 0.0011,0.0013) //right
-    private val bias = arrayOf(0.411, 0.1106, 0.6439, 0.153, -0.0914, 0.4504)
+//    private val weight = arrayOf(0.0015, 0.0017, 0.0017, 0.0016, 0.0015, 0.0015) //  male left
+//    private val bias = arrayOf(-0.1376, -0.6738, -0.1653, -0.8853, -0.5407, -0.3266)
+//    private val weight = arrayOf(0.0016, 0.0015, 0.0015, 0.0016, 0.0015,0.0018) // male right
+//    private val bias = arrayOf(-0.4404, -1.1027, -0.7006, -0.693, -0.8453, -0.3081)
+
+    private val weight = arrayOf(0.0004, 0.0003, 0.0004, 0.0004, 0.0004, 0.0002) //  female left
+    private val bias = arrayOf(0.0214, 0.0206, 0.0523, 0.0085, 0.0171, 0.0351)
+//    private val weight = arrayOf(0.0004, 0.0003, 0.0003, 0.0003, 0.0003,0.0004) // female right
+//    private val bias = arrayOf(0.0578, 0.0367, 0.0538, 0.053, 0.0634, -0.0621)
+
 //    private var mutableListOfDouble: MutableList<Double> = mutableListOf()
     private var datalist: LinkedList<Double> = LinkedList()
+    private var PressureList: LinkedList<Double> = LinkedList()
+    private var PressureAverageList: LinkedList<Double> = LinkedList()
 //    private var storageOldData: MutableList<Double> = mutableListOf()
 
 //    private val weight = arrayOf(1, 1, 1, 1, 1,1) // calibrate
@@ -217,9 +283,9 @@ class DataViewer: AppCompatActivity() {
 //
 //    private fun gyrTransform(value: Short): Float = value.toFloat() / 32767f * 250f
 //    private fun gyrTransform(value: Float): Float = value / 32767f * 250f
-    private fun appendData(data: Double){
+    private fun appendData(list: LinkedList<Double>,data: Double){
 
-        datalist.add(data)
+        list.add(data)
 
 
     }
@@ -274,16 +340,24 @@ class DataViewer: AppCompatActivity() {
 
         analysisButton.setOnClickListener {
             MaterialAlertDialogBuilder(this).apply {
-                var velocity = (10 / ((filteredPeaks2.toMutableList().last().toDouble()-filteredPeaks2.toMutableList().first().toDouble())/1000))
-                var average_peak_value = height.average()
-                for (i in 0 until  filteredPeaks2.toMutableList().size-1){
-                    step_duration.add(i,filteredPeaks2.get(i+1).toDouble()-filteredPeaks2.get(i).toDouble() )
-                }
+                if(filteredPeaks_t.isNotEmpty()) {
+                    var velocity = (10 / ((filteredPeaks_t.toMutableList().last()
+                        .toDouble() - filteredPeaks_t.toMutableList().first().toDouble()) / 1000))
+                    var average_peak_value = height.average()
+                    for (i in 0 until filteredPeaks_t.toMutableList().size - 1) {
+                        step_duration.add(i,
+                            filteredPeaks_t.get(i + 1).toDouble() - filteredPeaks_t.get(i)
+                                .toDouble()
+                        )
+                    }
 //                step_duration.add(0,(filteredPeaks1.get(1).toDouble()-filteredPeaks1.get(0).toDouble()))
-                var step_duration_average = step_duration.average()
-                emg.updataAnalysisData(velocity,step_duration_average,average_peak_value)
-                Log.i("velocity", velocity.toString())
-
+                    var step_duration_average = step_duration.average()
+                    emg.updataAnalysisData(velocity, step_duration_average, average_peak_value)
+                    Log.i("velocity", velocity.toString())
+                }
+                else{
+                    Toast.makeText(this@DataViewer,"no peak data",Toast.LENGTH_SHORT).show()
+                }
             }
         }
 
